cdfc8298a9ec0edcf4aef8508ba1a6a44575b014
Eugene Fedchuk <fed4chuck@gmail.com>
2023-04-05 17:19:38 +0300

    Serializer resembles the Rust version more closely

--- include/json.hpp
+++ include/json.hpp
@@ -8,46 +8,67 @@
 #include "error.hpp"
 #include "result.hpp"
 #include "datatype.hpp"
+#include "str.hpp"
 
 namespace json {
     namespace ser {
         struct Serializer {
+            using SerializeStruct = Serializer;
+            using Ok = struct {};
+            using Err = error::Error;
+
+            // Result alias
+            template<typename T>
+            struct Result : result::Result<T, Err> {
+                Result(error::Error e) : result::Result<T, Err>(e) {}
+                Result(T ok) : result::Result<T, Err>(ok) {}
+            };
+
             std::string output;
-            void serialize(bool &v) {
+
+            Result<Ok> serialize(bool &v) {
                 this->output += v ? "true" : "false";
+                return Ok();
             }
-            void serialize(int &v) {
+            Result<Ok> serialize(int &v) {
                 this->output += std::to_string(v);
+                return Ok();
             }
-            void serialize(const char *v) {
+            Result<Ok> serialize(const char *v) {
                 this->output += "\"";
                 this->output += v;
                 this->output += "\"";
+                return Ok();
             }
             template<typename T>
-            void serialize(T &v) {
+            Result<Ok> serialize(T &v) {
                 v.serialize(*this);
+                return Ok();
             }
-            Serializer *serialize_struct(const char *name) {
+            Result<SerializeStruct *> serialize_struct() {
                 this->output += "{";
                 return this;
             }
             template<typename T>
-            void serialize_field(const char *key, T &value) {
+            Result<SerializeStruct *> serialize_field(const char *key, T &value) {
                 if (output.back() != '{') {
                     this->output += ",";
                 }
                 serialize(key);
                 this->output += ":";
                 serialize(value);
+                return this;
             }
-            void end() {
+            Result<Ok> end() {
                 this->output += "}";
+                return Ok();
             }
         };
     }
     template<typename T>
-    std::string from(T &value) {
+    result::Result<std::string, error::Error>
+    // std::string
+    from(T &value) {
         ser::Serializer serializer;
         serializer.serialize(value);
         return serializer.output;
@@ -88,23 +109,39 @@ namespace json {
             }
             template<typename T>
             Result<T> parse_unsigned() {
-                char ch = TRY(this->peek_char());
+                char ch = TRY(this->next_char());
                 if (ch <= '0' || ch >= '9') return Error("Expected unsigned integer");
                 T res = ch - '0';
                 while (true) {
-                    match(this->peek_char().ok()) {{
-                        of(Some, ch)
-                            if (ch <= '0' || ch >= '9') {
-                                return res;
-                            } else {
-                                this->input++;
-                                res *= 10;
-                                res += ch - '0';
-                            }
-                        of(None) return res;
+                    match(this->peek_char()) {{
+                        of(Ok, (ch, (ch >= '0' && ch <= '9'))) {
+                            this->input++;
+                            res *= 10;
+                            res += ch - '0';
+                        }
+                        of_default { return res; }
                     }}
                 }
             }
+            template<typename T>
+            Result<T> parse_signed() {
+                bool neg = TRY(this->peek_char()) == '-';
+                if (neg) this->input++;
+                T res = TRY(this->parse_unsigned<T>());
+                return neg ? -res : res;
+            }
+            Result<str> parse_string() {
+                if (TRY(this->next_char()) != '"') return Error("Expected string");
+                const char *end = strchr(this->input, '"');
+                if (end == NULL) return Error("EOF");
+                str res(this->input, end - this->input);
+                this->input = end + 1;
+                return res;
+            }
+        };
+        template<typename T>
+        class Visitor {
+            // TODO
         };
     }
     template<typename T>
@@ -114,13 +151,20 @@ namespace json {
     }
 }
 
-#define _SER_FIELD(field, ...) serialize_struct->serialize_field(#field, field);
+#define _SER_FIELD(field, ...) TRY(ser->serialize_field(#field, field));
 
-#define SERIALIZE(T, ...)                                        \
-    void serialize(json::ser::Serializer &serializer) {          \
-        auto serialize_struct = serializer.serialize_struct(#T); \
-        FOREACH(_SER_FIELD, __VA_ARGS__)                         \
-        serialize_struct->end();                                 \
+/**
+ * Specify `Serializer` with `using`
+ * @params comma separated field names to serialize
+ */
+#define SERIALIZE(...)                                 \
+    struct Unit {};                                    \
+    result::Result<Unit, error::Error>                 \
+    serialize(Serializer &serializer) {                \
+        auto ser = TRY(serializer.serialize_struct()); \
+        FOREACH(_SER_FIELD, __VA_ARGS__);              \
+        TRY(ser->end());                               \
+        return Unit();                                 \
     }
 
 #endif // JSON_H_
