da2acbce984ecb914193584e618c14919b979dce
Eugene Fedchuk <fed4chuck@gmail.com>
2023-06-01 00:23:25 +0300

    Removed the concepts (for now)

--- include/serde.hpp
+++ include/serde.hpp
@@ -1,92 +1,62 @@
 #ifndef SERDE_H_
 #define SERDE_H_
 
-#include "result.hpp"
-#include "str.hpp"
+#include "fst.hpp"
 
-// Ideally I want everything to be traits and not classes to inherit
 
-template<typename T, typename S>
-concept Serialize = requires(T &value, S &serializer)
-{
-    value.serialize(serializer);
-};
+namespace serde::de {
+    using namespace fst;
 
-// template<typename T>
-// concept SerializeStruct = requires(T a, const char *key, )
-// {
-//     a.serialize_field(key, value);
-// };
+    using result::Err;
+    template<typename T>
+    using Result = result::Result<T, error::Error>;
 
-// template<typename D, typename V>
-// concept Deserializer = requires(D &deserializer, V &visitor)
-// {
-//     deserializer.deserialize_bool(visitor);
-//     deserializer.deserialize_short(visitor);
-//     deserializer.deserialize_ushort(visitor);
-//     deserializer.deserialize_int(visitor);
-//     deserializer.deserialize_uint(visitor);
-//     deserializer.deserialize_long(visitor);
-//     deserializer.deserialize_ulong(visitor);
-//     deserializer.deserialize_long_long(visitor);
-//     deserializer.deserialize_ulong_long(visitor);
-//     deserializer.deserialize_str(visitor);
-//     deserializer.deserialize_identifier(visitor);
-// };
+    template<typename T>
+    struct Visitor {
+        using Value = T;
 
-namespace serde {
-    namespace de {
-        using result::Err;
-        template<typename T>
-        using Result = result::Result<T, error::Error>;
-
-        template<typename T>
-        struct Visitor {
-            using Value = T;
-
-            virtual Result<Value> visit_bool(bool value) {
-                return Err(error::Error("Invalid type: Unexpected Bool"));
-            }
-            virtual Result<Value> visit_short(short value) {
-                return this->visit_long_long(value);
-            }
-            virtual Result<Value> visit_int(int value) {
-                return this->visit_long_long(value);
-            }
-            virtual Result<Value> visit_long(long value) {
-                return this->visit_long_long(value);
-            }
-            virtual Result<Value> visit_long_long(long long value) {
-                return Err(error::Error("Invalid type: Unexpected Signed"));
-            }
-            virtual Result<Value> visit_ushort(unsigned short value) {
-                return this->visit_long_long(value);
-            }
-            virtual Result<Value> visit_uint(unsigned int value) {
-                return this->visit_long_long(value);
-            }
-            virtual Result<Value> visit_ulong(unsigned long value) {
-                return this->visit_long_long(value);
-            }
-            virtual Result<Value> visit_ulong_long(unsigned long long value) {
-                return Err(error::Error("Invalid type: Unexpected Unsigned"));
-            }
-            virtual Result<Value> visit_float(float value) {
-                return this->visit_double(value);
-            }
-            virtual Result<Value> visit_double(double value) {
-                return Err(error::Error("Invalid type: Unexpected Float"));
-            }
-            virtual Result<Value> visit_str(str value) {
-                return Err(error::Error("Invalid type: Unexpected str"));
-            }
-            // idk, feels wrong and broken
-            template<typename A>
-            Result<Value> visit_map(A map) {
-                return Err(error::Error("Invalid type: Unexpected map"));
-            }
-        };
-    }
+        virtual Result<Value> visit_bool(bool value) {
+            return Err(error::Error("Invalid type: Unexpected Bool"));
+        }
+        virtual Result<Value> visit_short(short value) {
+            return this->visit_long_long(value);
+        }
+        virtual Result<Value> visit_int(int value) {
+            return this->visit_long_long(value);
+        }
+        virtual Result<Value> visit_long(long value) {
+            return this->visit_long_long(value);
+        }
+        virtual Result<Value> visit_long_long(long long value) {
+            return Err(error::Error("Invalid type: Unexpected Signed"));
+        }
+        virtual Result<Value> visit_ushort(unsigned short value) {
+            return this->visit_long_long(value);
+        }
+        virtual Result<Value> visit_uint(unsigned int value) {
+            return this->visit_long_long(value);
+        }
+        virtual Result<Value> visit_ulong(unsigned long value) {
+            return this->visit_long_long(value);
+        }
+        virtual Result<Value> visit_ulong_long(unsigned long long value) {
+            return Err(error::Error("Invalid type: Unexpected Unsigned"));
+        }
+        virtual Result<Value> visit_float(float value) {
+            return this->visit_double(value);
+        }
+        virtual Result<Value> visit_double(double value) {
+            return Err(error::Error("Invalid type: Unexpected Float"));
+        }
+        virtual Result<Value> visit_str(str value) {
+            return Err(error::Error("Invalid type: Unexpected str"));
+        }
+        // idk, feels wrong and broken
+        template<typename A>
+        Result<Value> visit_map(A map) {
+            return Err(error::Error("Invalid type: Unexpected map"));
+        }
+    };
 }
 
 #define _SER_FIELD(field) TRY(ser->serialize_field(#field, field));
@@ -97,17 +67,45 @@ namespace serde {
  */
 #define SERIALIZE(NAME, ...)                                \
     template<typename S>                                    \
-    result::Result<void, error::Error>                      \
+    fst::result::Result<void, fst::error::Error>            \
     serialize(S &serializer) {                              \
-        using result::Ok;                                   \
-        using result::Err;                                  \
+        using fst::result::Ok;                              \
+        using fst::result::Err;                             \
         auto ser = TRY(serializer.serialize_struct(#NAME)); \
         FOREACH(_SER_FIELD, __VA_ARGS__);                   \
         TRY(ser->end());                                    \
         return Ok();                                        \
     }
 
-#define DESERIALIZE(...)
+#define FIELD_VISITOR(FIELD)     \
+    if (value == #FIELD) {       \
+        return Ok(Field::FIELD); \
+    }
 
+#define FIELD_NAME(X) #X,
+
+#define DESERIALIZE(NAME, ...)                                                  \
+    template<typename D>                                                        \
+    fst::result::Result<RGB, typename D::Error> deserialize(D &deserializer) {  \
+        using namespace fst::result;                                            \
+        struct FieldImpl {                                                      \
+            Result<Field, typename D::Error> deserialize(D &deserializer) {     \
+                struct FieldVisitor : serde::de::Visitor<Field> {               \
+                    using Value = Field;                                        \
+                    fst::result::Result<Value, fst::error::Error>               \
+                    visit_str(fst::str value) override {                        \
+                        FOREACH(FIELD_VISITOR, __VA_ARGS__)                     \
+                        return Err(fst::error::Error("Unknown field"));         \
+                    }                                                           \
+                };                                                              \
+                return deserializer.deserialize_identifier(FieldVisitor());     \
+            }                                                                   \
+        };                                                                      \
+        const char *FIELDS[] = {                                                \
+            FOREACH(FIELD_NAME, __VA_ARGS__)                                    \
+        };                                                                      \
+        return deserializer.deserialize_struct(#NAME, FIELDS, NAME##Visitor()); \
+    }                                                                           \
+    enum class Field { __VA_ARGS__ }
 
 #endif // SERDE_H_
